cb3ab7849468a1211ab6bb4eebf7166cd5dfecbb
bayzman <bassmaninno102@gmail.com>
2024-01-30 20:56:26 -0800

    Regular expressions in Ruby

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Regular expression to match htbn and hbn
+
+puts ARGV[0].scan(/hbt+n/)
---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Regular expression to match numbers
+
+puts ARGV[0].scan(/^\d{10}$/)
