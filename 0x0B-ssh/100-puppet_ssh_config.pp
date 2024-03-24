85aee87f2150b99e8f657082fc6610883f912a9b
bayzman <bassmaninno102@gmail.com>
2024-03-24 15:56:25 -0700

    Use a private key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,15 +3,15 @@
 include stdlib
 
 file_line { 'Turn off passwd auth':
-	    ensure => present,
-	    path => 'etc/ssh/ssh_config',
-	    line => 'PasswordAuthentication no',
-	    replace => true,
+	  ensure => present,
+	  path => 'etc/ssh/ssh_config',
+	  line => 'PasswordAuthentication no',
+	  replace => true
 }
 
 file_line { 'Declare identity file':
-	    ensure => present,
-	    path => 'etc/ssh/ssh_config',
-	    line => 'Identityfile ~/.ssh/school',
-	    replace => true,
+	  ensure => present,
+	  path => 'etc/ssh/ssh_config',
+	  line => 'Identityfile ~/.ssh/school',
+	  replace => true
 }
