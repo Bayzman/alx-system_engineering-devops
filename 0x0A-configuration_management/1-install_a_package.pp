1c6e187d14abeba79e4eeacb0729e173f38b46ec
bayzman <bassmaninno102@gmail.com>
2024-03-27 19:16:05 -0700

    install a package fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # installs flask using pip3 with puppet
+
 package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
