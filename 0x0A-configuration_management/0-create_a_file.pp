0e131b7d0b316c5e8fb37829aa9fd50e7120eb55
bayzman <bassmaninno102@gmail.com>
2024-03-27 19:00:48 -0700

    create a file fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file in tmp folder with puppet
 file {'/tmp/school':
-      path => '/tmp/school',
-      mode => '0744',
-      owner => 'www-data',
-      group => 'www-data',
+      path    => '/tmp/school',
+      mode    => '0744',
+      owner   => 'www-data',
+      group   => 'www-data',
       content => 'I love Puppet',
       }
