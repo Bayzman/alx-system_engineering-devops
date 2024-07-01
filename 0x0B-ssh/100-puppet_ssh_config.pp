85569fc2b850df034c6d49ede5e96256f34b1a83
Bayzman <bassmaninno102@gmail.com>
2024-06-30 21:51:02 -0700

    Client configuration file puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,19 @@
+# config file
+
+include stdlib
+
+file_line { 'Turn off Password Auth':
+  path             => '/etc/ssh/ssh_config',
+  line             => 'PasswordAuthentication no',
+  match            => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace          => true,
+  append_on_no_match => true
+}
+
+file_line { 'Declare Identity file':
+  path               => '/etc/ssh/ssh_config',
+  line               => 'IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
