b2ff26b75282597f422e5a6948354fcebf128e34
Bayzman <ndukweikechi@gmail.com>
2024-08-19 14:50:59 -0700

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,13 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "request"}
+    headers = {"User-Agent": "Bayzman"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-        return (0)
+        return 0
 
     else:
         data = response.json()
         subs = data['data']['subscribers']
-        return (subs)
+        return subs
