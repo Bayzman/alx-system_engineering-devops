b7ae609df3c4fbc0b87bf935ec02c549848458fb
Bayzman <ndukweikechi@gmail.com>
2024-08-19 14:47:14 -0700

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "request"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
